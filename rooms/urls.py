79674a8c880a622ae6f2722aba357765edf5a5e3
2018008068 <aza1200@naver.com>
2021-09-13 22:06:26 +0900

    serializer

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
+#from rest_framework.routers import DefaultRouter
+from django.urls import path,include
+#from . import viewsets
 from . import views
 
 app_name = "rooms"
 
+# router = DefaultRouter()
+# router.register("",viewsets.RoomViewset,basename='room')
+
+#urlpatterns =router.urls
+
 urlpatterns = [
-    path("list/",views.ListRoomsView.as_view()),
+    #path("",views.ListRoomsView.as_view()),
+    path("",views.rooms_view),
     path("<int:pk>/",views.SeeRoomView.as_view()),
     #path("<int:jhkim>/",views.SeeRoomView.as_view()),
-    #pk 라는 키워드가 있기에 restapi 가 알아먹음
-    #이거 고칠려면 listapiview,retreive api view  의 lookup_url_kwarg
+#     # #pk 라는 키워드가 있기에 restapi 가 알아먹음
+#     # #이거 고칠려면 listapiview,retreive api view  의 lookup_url_kwarg
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Room
-from .serializers import RoomSerializer,BigRoomSerializer
+from .serializers import ReadRoomSerializer,WriteRoomSerializer
 
 # @api_view(["GET"])
 # def list_rooms(request):
@@ -23,13 +25,27 @@ from .serializers import RoomSerializer,BigRoomSerializer
 
 #오직 하나의 뷰만 페이지 사이즈를 바꾸고 싶을떄???
 # 강의 1.4 참조
-class ListRoomsView(ListAPIView):
-    #많은 것을 커스터마이징할 필요가 없을때의 뷰
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+# class ListRoomsView(ListAPIView):
+#     #많은 것을 커스터마이징할 필요가 없을때의 뷰
+#     queryset = Room.objects.all()
+#     serializer_class = RoomSerializer
 
+
+#api view 를 이용시 django가 view 를 처리하는 방식을 바꿀수있당
+@api_view(["GET","POST"])
+def rooms_view(request):
+    if request.method == "GET":
+        rooms = Room.objects.all()
+        serializer= ReadRoomSerializer(rooms,many=True).data
+        return Response(serializer)
+    elif request.method == "POST":
+        serializer = WriteRoomSerializer(data= request.data)
+        if serializer.is_valid():
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 class SeeRoomView(RetrieveAPIView):
 
     queryset = Room.objects.all()
-    serializer_class = BigRoomSerializer
+    serializer_class = ReadRoomSerializer
     #lookup_url_kwarg = "jhkim"
